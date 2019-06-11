3521b60274afd72bdfc04ee92288302fb0ea6bc4
Akshat Nayak <akshatnayak197@gmai.com>
2019-06-11 17:17:55 +0530

    Creating Migration,Model,Controller,Factories

---
+++ database/factories/LikesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Likes;
+use Faker\Generator as Faker;
+
+$factory->define(Likes::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
